dd4e6490002a118d99999f122838440e9a546538
Amit Tambulkar <amittambulkar104@gmail.com>
2023-08-25 21:37:31 +0530

    Added new feature
    Only one symbol will be used from the list of symbols.

--- random passwprd generator V1.py
+++ random passwprd generator V1.py
@@ -1,16 +1,28 @@
 # Build a random password generator. It should include characters, numbers and symbols and the length should be 8 min.
+# But it should use any one symbol only once. And only one uppercase
 
 import random
 import string
 
-chrac = string.ascii_letters
-num = string.digits
-sym = "!@#$%^&*?"
+characters = string.ascii_letters
+numbers = string.digits
+symbols = "!@#$%^&*?"
 
-combo = chrac + num + sym
+# Only one symbol needs to be added in the generated password
+sym = random.choice(symbols)
+remaining_sym = symbols.replace(sym,'')
 
 random_num = random.randint(8, 12)
+length = random.randint(8, 12)
+combo = characters + numbers
 
-Password = ''.join(random.choice(combo) for _ in range(random_num))
+# Create a list of password only with charaters and numbers.
+Password1 = [random.choice(combo) for _ in range(length -1)]
+
+# Add any one random symbol in the password 
+Position1 = random.randint(0, length -1)
+Password1.insert(Position1, sym)
+
+Password = ''.join(Password1)
 
 print("Here is your password: ", Password)
